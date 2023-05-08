cb15ade98375c549cd805e934d9fe5066f7e4638
SUGAPRIYAN P K <sugapriyan2003@gmail.com>
2023-05-08 19:13:26 +0530

    Update listComprehensions.py

--- BasicDatatypes/listComprehensions.py
+++ BasicDatatypes/listComprehensions.py
@@ -1,1 +1,7 @@
-
+if __name__ == '__main__':
+    x = int(input())
+    y = int(input())
+    z = int(input())
+    n = int(input())
+    num = [[i,j,k] for i in range(x+1) for j in range(y+1) for k in range(z+1) if (i+j+k)!=n] # all permutations of x,y,z and those sums are not equal to n would store in num array
+    print(num)
