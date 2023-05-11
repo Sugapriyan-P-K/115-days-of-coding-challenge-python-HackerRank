c2c66be16c29906439631b90180669fa1eb3e239
SUGAPRIYAN P K <sugapriyan2003@gmail.com>
2023-05-11 10:29:47 +0530

    Create Day11FindingThePercentage.py

---
+++ BasicDatatypes/Day11FindingThePercentage.py
@@ -0,0 +1,10 @@
+if __name__ == '__main__':
+    n = int(input())
+    student_marks = {}
+    for _ in range(n):
+        name, *line = input().split()
+        scores = list(map(float, line))
+        student_marks[name] = scores
+    query_name = input()
+    query_scores = student_marks[query_name]
+    print("{0:.2f}".format(sum(query_scores)/(len(query_scores))))
