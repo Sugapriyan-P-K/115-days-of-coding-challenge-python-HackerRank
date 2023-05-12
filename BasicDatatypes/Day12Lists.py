4463246c900bd1bab8d92f9a08fe80c71d8a85d2
SUGAPRIYAN P K <sugapriyan2003@gmail.com>
2023-05-12 19:12:19 +0530

    Update Day12Lists.py

--- BasicDatatypes/Day12Lists.py
+++ BasicDatatypes/Day12Lists.py
@@ -1,1 +1,19 @@
-
+if __name__ == '__main__':
+    N = int(input())
+    list = []
+    for i in range(N):
+        args = input().strip().split(" ")
+        if args[0] == "append":
+            list.append(int(args[1]))
+        elif args[0] == "insert":
+            list.insert(int(args[1]), int(args[2]))
+        elif args[0] == "print":
+            print(list)
+        elif args[0] == "remove":
+            list.remove(int(args[1]))
+        elif args[0] == "sort":
+            list.sort()
+        elif args[0] == "pop":
+            list.pop()
+        elif args[0] == "reverse":
+            list.reverse()
