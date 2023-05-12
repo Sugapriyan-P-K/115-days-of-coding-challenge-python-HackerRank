9ffa81b2acd803d00c7f974f7957ab9c43369369
SUGAPRIYAN P K <sugapriyan2003@gmail.com>
2023-05-12 19:13:04 +0530

    Update Day12Lists.py

--- BasicDatatypes/Day12Lists.py
+++ BasicDatatypes/Day12Lists.py
@@ -17,3 +17,15 @@ if __name__ == '__main__':
             list.pop()
         elif args[0] == "reverse":
             list.reverse()
+            
+   
+# Consider a list (list = []). You can perform the following commands:
+
+# insert i e: Insert integer  at position .
+# print: Print the list.
+# remove e: Delete the first occurrence of integer .
+# append e: Insert integer  at the end of the list.
+# sort: Sort the list.
+# pop: Pop the last element from the list.
+# reverse: Reverse the list.
+# Initialize your list and read in the value of  followed by  lines of commands where each command will be of the 7 types listed above. Iterate through each command in order and perform the corresponding operation on your list.
