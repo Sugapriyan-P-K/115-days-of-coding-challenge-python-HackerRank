de5d6244b70cc374b5960c20be4d330e74638bb0
SUGAPRIYAN P K <sugapriyan2003@gmail.com>
2023-05-16 10:29:36 +0530

    Update Day16WhatsYourName.py

--- Strings/Day16WhatsYourName.py
+++ Strings/Day16WhatsYourName.py
@@ -1,1 +1,16 @@
+# The function is expected to return a STRING.
+# The function accepts following parameters:
+#  1. STRING first
+#  2. STRING last
+#
 
+def print_full_name(first, last):
+    # Write your code here
+    print("Hello " + first +" " + last+"! You just delved into python.")
+
+
+if __name__ == '__main__':
+    first_name = input()
+    last_name = input()
+    print_full_name(first_name, last_name)
+ 
