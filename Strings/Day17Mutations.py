cfc05f688ac31ec264c3db50a769669ef780cc24
SUGAPRIYAN P K <sugapriyan2003@gmail.com>
2023-05-17 19:58:22 +0530

    Update Day17Mutations.py

--- Strings/Day17Mutations.py
+++ Strings/Day17Mutations.py
@@ -1,1 +1,36 @@
+def mutate_string(string, position, character):
+    l=list(string)
+    l[position]=character
+    string=''.join(l)
+    return string
 
+if __name__ == '__main__':
+    s = input()
+    i, c = input().split()
+    s_new = mutate_string(s, int(i), c)
+    print(s_new)
+ 
+# Read a given string, change the character at a given index and then print the modified string.
+# Function Description
+
+# Complete the mutate_string function in the editor below.
+
+# mutate_string has the following parameters:
+
+# string string: the string to change
+# int position: the index to insert the character at
+# string character: the character to insert
+# Returns
+
+# string: the altered string
+# Input Format
+
+# The first line contains a string, string.
+# The next line contains an integer position, the index location and a string character, separated by a space.
+
+# Sample Input
+
+# STDIN           Function
+# -----           --------
+# abracadabra     s = 'abracadabra'
+# 5 k             position = 5, character = 'k'
