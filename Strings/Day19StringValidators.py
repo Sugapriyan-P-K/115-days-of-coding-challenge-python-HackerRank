53290b71e8872f7d5718598050730a6c39f62455
SUGAPRIYAN P K <sugapriyan2003@gmail.com>
2023-05-20 11:30:38 +0530

    Update Day19StringValidators.py

--- Strings/Day19StringValidators.py
+++ Strings/Day19StringValidators.py
@@ -1,1 +1,21 @@
-
+if __name__ == '__main__':
+    s = input()
+    print(any([i.isalnum() for i in s]))
+    print(any([i.isalpha() for i in s]))
+    print(any([i.isdigit() for i in s]))
+    print(any([i.islower() for i in s]))
+    print(any([i.isupper() for i in s])
+     
+# Task
+# You are given a string .
+# Your task is to find out if the string  contains: alphanumeric characters, alphabetical characters, digits, lowercase and uppercase characters.
+# Input Format
+# A single line containing a string S.
+# Constraints
+# 0 < len(S) < 1000 
+# Output Format
+# In the first line, print True if S has any alphanumeric characters. Otherwise, print False.
+# In the second line, print True if S has any alphabetical characters. Otherwise, print False.
+# In the third line, print True if S has any digits. Otherwise, print False.
+# In the fourth line, print True if S has any lowercase characters. Otherwise, print False.
+# In the fifth line, print True if S has any uppercase characters. Otherwise, print False.
