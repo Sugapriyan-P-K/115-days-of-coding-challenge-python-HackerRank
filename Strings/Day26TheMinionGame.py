8d9c0bb2f6fb5acb8e0c00b063a457b5d0af875f
SUGAPRIYAN P K <sugapriyan2003@gmail.com>
2023-05-26 11:28:35 +0530

    Create Day26TheMinionGame.py

---
+++ Strings/Day26TheMinionGame.py
@@ -0,0 +1,21 @@
+# Kevin and Stuart want to play the 'The Minion Game'.
+
+# Game Rules
+
+# Both players are given the same string, S.
+# Both players have to make substrings using the letters of the string S.
+# Stuart has to make words starting with consonants.
+# Kevin has to make words starting with vowels.
+# The game ends when both players have made all possible substrings.
+
+# Scoring
+# A player gets +1 point for each occurrence of the substring in the string S.
+
+# For Example:
+# String S = BANANA
+# Kevin's vowel beginning word = ANA
+# Here, ANA occurs twice in BANANA. Hence, Kevin will get 2 Points.
+# Sample Input
+# BANANA
+# Sample Output
+# Stuart 12
