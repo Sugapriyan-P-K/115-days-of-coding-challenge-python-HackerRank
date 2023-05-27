6a2da77e4a1b1c2ebebf0cbab81bf6aec377f275
SUGAPRIYAN P K <sugapriyan2003@gmail.com>
2023-05-27 10:59:24 +0530

    Create Day27MergeTheTools.py

---
+++ Strings/Day27MergeTheTools.py
@@ -0,0 +1,23 @@
+
+# Example
+# s = 'AAABCADDE'
+# k=3
+# There are three substrings of length  to consider: 'AAA', 'BCA' and 'DDE'. The first substring is all 'A' characters, so u1='A'. The second substring has all distinct characters, so u2='BCA'. 
+# The third substring has 2 different characters, so u3 = 'DE'. Note that a subsequence maintains the original order of characters encountered. The order of characters in each subsequence shown is important.
+
+# Function Description
+
+# Complete the merge_the_tools function in the editor below.
+
+# merge_the_tools has the following parameters:
+
+#   string s: the string to analyze
+#   int k: the size of substrings to analyze
+# Prints
+
+# Print each subsequence on a new line. There will be n/k of them. No return value is expected.
+
+# Input Format
+
+# The first line contains a single string, s.
+# The second line contains an integer, k, the length of each substring.
