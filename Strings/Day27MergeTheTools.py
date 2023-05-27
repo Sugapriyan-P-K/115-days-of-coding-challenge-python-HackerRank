881f7d9f57e9bc24369cb6e7966672a5a8d6bbc9
SUGAPRIYAN P K <sugapriyan2003@gmail.com>
2023-05-27 11:01:30 +0530

    Update Day27MergeTheTools.py

--- Strings/Day27MergeTheTools.py
+++ Strings/Day27MergeTheTools.py
@@ -14,18 +14,12 @@ if __name__ == '__main__':
 # The third substring has 2 different characters, so u3 = 'DE'. Note that a subsequence maintains the original order of characters encountered. The order of characters in each subsequence shown is important.
 
 # Function Description
-
-# Complete the merge_the_tools function in the editor below.
-
 # merge_the_tools has the following parameters:
-
 #   string s: the string to analyze
 #   int k: the size of substrings to analyze
 # Prints
-
 # Print each subsequence on a new line. There will be n/k of them. No return value is expected.
 
 # Input Format
-
 # The first line contains a single string, s.
 # The second line contains an integer, k, the length of each substring.
