2029f70228a636b7f719b5653768d29b6d35c659
SUGAPRIYAN P K <sugapriyan2003@gmail.com>
2023-05-27 11:00:45 +0530

    Update Day27MergeTheTools.py

--- Strings/Day27MergeTheTools.py
+++ Strings/Day27MergeTheTools.py
@@ -1,3 +1,11 @@
+def merge_the_tools(string, k):
+    for i in range(0, len(string),k):
+        s=string[i:i+k]
+        print(''.join(sorted(set(s),key=s.index)))
+
+if __name__ == '__main__':
+    string, k = input(), int(input())
+    merge_the_tools(string, k)
 
 # Example
 # s = 'AAABCADDE'
