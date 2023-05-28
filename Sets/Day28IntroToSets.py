e06680ad911ac99753dcc3a25afeb5f19f9c9069
SUGAPRIYAN P K <sugapriyan2003@gmail.com>
2023-05-28 12:41:08 +0530

    solved Day28IntroToSets.py

--- Sets/Day28IntroToSets.py
+++ Sets/Day28IntroToSets.py
@@ -1,4 +1,16 @@
+def average(array):
+    sum1=0
+    res= set(array)
+    for i in res:  
+        sum1=sum1+i
+    avg=float(sum1/len(res))
+    return float(avg)
 
+if __name__ == '__main__':
+    n = int(input())
+    arr = list(map(int, input().split()))
+    result = average(arr)
+    print(result)
 
 # Ms. Gabriel Williams is a botany professor at District College. One day, she asked her student Mickey to compute the average of all the plants with distinct heights in her greenhouse.
 # Function Description
