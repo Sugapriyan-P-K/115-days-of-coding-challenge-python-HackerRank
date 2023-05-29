fdb1f966c769de91a79f72afd1276dd32bc9cd84
SUGAPRIYAN P K <sugapriyan2003@gmail.com>
2023-05-29 11:15:22 +0530

    Create Day29NoIdea.py

---
+++ Sets/Day29NoIdea.py
@@ -0,0 +1,15 @@
+
+
+# There is an array of n integers. There are also 2 disjoint sets, A and B, each containing m integers. You like all the integers in set A and dislike all the integers in set B.
+# Your initial happiness is 0. For each i integer in the array, if i ∈ A, you add 1 to your happiness. If i ∈ B, you add -1 to your happiness. Otherwise, your happiness does not change.
+# Output your final happiness at the end.
+
+# Note: Since A and B are sets, they have no repeated elements. However, the array might contain duplicate elements.
+  
+# Sample Input
+# 3 2
+# 1 5 3
+# 3 1
+# 5 7
+# Sample Output
+# 1
