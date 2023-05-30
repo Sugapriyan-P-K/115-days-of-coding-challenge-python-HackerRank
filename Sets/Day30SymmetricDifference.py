0ef32200a2980b6972e776bfbc5ce6d655184e60
SUGAPRIYAN P K <sugapriyan2003@gmail.com>
2023-05-30 11:40:06 +0530

    Create Day30SymmetricDifference.py

---
+++ Sets/Day30SymmetricDifference.py
@@ -0,0 +1,17 @@
+
+
+# Task
+# Given 2 sets of integers, M and N, print their symmetric difference in ascending order. The term symmetric difference indicates those values that exist in either M or 
+# N but do not exist in both.
+# Sample Input
+# STDIN       Function
+# -----       --------
+# 4           set a size M = 4
+# 2 4 5 9     a = {2, 4, 5, 9}
+# 4           set b size N = 4
+# 2 4 11 12   b = {2, 4, 11, 12}
+# Sample Output
+# 5
+# 9
+# 11
+# 12
