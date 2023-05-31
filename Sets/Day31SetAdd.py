ef84fa09a4182bf37059f3badebf5a91a8022505
SUGAPRIYAN P K <sugapriyan2003@gmail.com>
2023-05-31 11:28:19 +0530

    Create Day31SetAdd.py

---
+++ Sets/Day31SetAdd.py
@@ -0,0 +1,21 @@
+
+
+
+# Task
+# Apply your knowledge of the .add() operation to help your friend Rupal.
+# Rupal has a huge collection of country stamps. She decided to count the total number of distinct country stamps in her collection.
+# She asked for your help. You pick the stamps one by one from a stack of N country stamps.
+# Find the total number of distinct country stamps.
+# Sample Input
+# 7
+# UK
+# China
+# USA
+# France
+# New Zealand
+# UK
+# France 
+# Sample Output
+# 5
+# Explanation
+# UK and France repeat twice. Hence, the total number of distinct country stamps is 5 (five).
