145925f25029acd4e6ac86a3a2344bd278f0c827
SUGAPRIYAN P K <sugapriyan2003@gmail.com>
2023-06-01 19:21:01 +0530

    Update Day32SetDiscardRemovePop.py

--- Sets/Day32SetDiscardRemovePop.py
+++ Sets/Day32SetDiscardRemovePop.py
@@ -1,4 +1,14 @@
-
+n = int(input())
+s = set(map(int, input().split()))
+for i in range(int(input())):
+    a = (input().strip().split())
+    if a[0]=="pop":
+        s.pop()
+    if a[0]=="remove":
+        s.remove(int(a[1]))
+    if a[0]=="discard":
+        s.discard(int(a[1]))
+print(sum(s))
 
 
 # Task
