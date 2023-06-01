96427dea92286b87cc8868644ff6e6d0fdfa250c
SUGAPRIYAN P K <sugapriyan2003@gmail.com>
2023-06-01 19:19:56 +0530

    Create Day32SetDiscardRemovePop.py

---
+++ Sets/Day32SetDiscardRemovePop.py
@@ -0,0 +1,26 @@
+
+
+
+# Task
+# You have a non-empty set s, and you have to execute N commands given in N lines.
+# The commands will be pop, remove and discard.
+# Sample Input
+# 9
+# 1 2 3 4 5 6 7 8 9
+# 10
+# pop
+# remove 9
+# discard 9
+# discard 8
+# remove 7
+# pop 
+# discard 6
+# remove 5
+# pop 
+# discard 5
+# Sample Output
+# 4
+# Explanation
+# After completing these 10 operations on the set, we get set ([4]). Hence, the sum is 4.
+# Note: Convert the elements of set s to integers while you are assigning them. To ensure the proper input of the set, we have added the first two lines of code to 
+# the editor.
