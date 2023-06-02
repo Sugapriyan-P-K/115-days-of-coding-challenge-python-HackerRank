1d249328b02ae588f63f00f9d603d0f9fe691ff5
SUGAPRIYAN P K <sugapriyan2003@gmail.com>
2023-06-02 11:13:27 +0530

    Create Day33SetUnion.py

---
+++ Sets/Day33SetUnion.py
@@ -0,0 +1,12 @@
+
+# Sample Input
+# 9
+# 1 2 3 4 5 6 7 8 9
+# 9
+# 10 1 2 3 11 21 55 6 8
+# Sample Output
+# 13
+# Explanation
+# Roll numbers of students who have at least one subscription:
+# 1,2,3,4,5,6,7,8,9,10,11,21 and 55. Roll numbers: 1,2,3,6 and 8 are in both sets so they are only counted once.
+# Hence, the total is  students.
