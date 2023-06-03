e2d0fa931653af8157ceed90c3b87cd7450b6658
SUGAPRIYAN P K <sugapriyan2003@gmail.com>
2023-06-03 12:23:13 +0530

    Create Day34SetIntersection.py

---
+++ Sets/Day34SetIntersection.py
@@ -0,0 +1,13 @@
+
+
+# Sample Input
+# 9
+# 1 2 3 4 5 6 7 8 9
+# 9
+# 10 1 2 3 11 21 55 6 8
+# Sample Output
+# 5
+# Explanation
+# The roll numbers of students who have both subscriptions:
+# 1,2,3,6 and 8.
+# Hence, the total is 5 students.
