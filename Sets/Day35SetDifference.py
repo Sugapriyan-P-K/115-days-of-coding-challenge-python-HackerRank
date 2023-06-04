b2bb149aa643f31ff0cec2c893b6ac2ccab5dc4d
SUGAPRIYAN P K <sugapriyan2003@gmail.com>
2023-06-04 11:35:51 +0530

    Create Day35SetDifference.py

---
+++ Sets/Day35SetDifference.py
@@ -0,0 +1,18 @@
+
+
+# Task
+# Students of District College have a subscription to English and French newspapers. Some students have subscribed to only the English newspaper, 
+# some have subscribed to only the French newspaper, and some have subscribed to both newspapers.
+# You are given two sets of student roll numbers. One set has subscribed to the English newspaper, and one set has subscribed to the French newspaper. 
+# Your task is to find the total number of students who have subscribed to only English newspapers.
+# Sample Input
+# 9
+# 1 2 3 4 5 6 7 8 9
+# 9
+# 10 1 2 3 11 21 55 6 8
+# Sample Output
+# 4
+# Explanation
+# The roll numbers of students who only have English newspaper subscriptions are:
+# 4,5,7 and 9.
+# Hence, the total is 4 students.
