f6fd69338b223fc33f6669851e015184eaa50c21
SUGAPRIYAN P K <sugapriyan2003@gmail.com>
2023-06-05 19:30:34 +0530

    Create Day36SetSymmetricDifference.py

---
+++ Sets/Day36SetSymmetricDifference.py
@@ -0,0 +1,17 @@
+
+# Task
+# Students of District College have subscriptions to English and French newspapers. Some students have subscribed to English only, some have subscribed to 
+# French only, and some have subscribed to both newspapers. You are given two sets of student roll numbers. One set has subscribed to the English newspaper, and one 
+# set has subscribed to the French newspaper. Your task is to find the total number of students who have subscribed to either the English or the French newspaper but 
+# not both.
+# Sample Input
+# 9
+# 1 2 3 4 5 6 7 8 9
+# 9
+# 10 1 2 3 11 21 55 6 8
+# Sample Output
+# 8
+# Explanation
+# The roll numbers of students who have subscriptions to English or French newspapers but not both are:
+# 4,5,7,9,11,21 and 55.
+# Hence, the total is 8 students.
