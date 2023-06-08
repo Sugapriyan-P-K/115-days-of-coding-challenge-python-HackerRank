3c97d4107df24c63eb4af551dd71bc6232535b4a
SUGAPRIYAN P K <sugapriyan2003@gmail.com>
2023-06-08 10:57:28 +0530

    Solved Day39CheckSubset.py

--- Sets/Day39CheckSubset.py
+++ Sets/Day39CheckSubset.py
@@ -1,4 +1,6 @@
-
+for i in range(int(input())):
+    x, a , y, b = input(), set(input().split()), input(), set(input().split())
+    print(a.issubset(b))
 
 # You are given two sets, A and B.
 # Your job is to find whether set A is a subset of set B.
