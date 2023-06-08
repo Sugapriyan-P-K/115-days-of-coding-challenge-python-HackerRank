a6935aa6fb73fa43ecc22abd5333b4160a714f61
SUGAPRIYAN P K <sugapriyan2003@gmail.com>
2023-06-08 10:56:39 +0530

    Create Day39CheckSubset.py

---
+++ Sets/Day39CheckSubset.py
@@ -0,0 +1,29 @@
+
+
+# You are given two sets, A and B.
+# Your job is to find whether set A is a subset of set B.
+# If set A is subset of set B, print True.
+# If set A is not a subset of set B, print False.
+# Sample Input
+# 3
+# 5
+# 1 2 3 5 6
+# 9
+# 9 8 5 6 3 2 1 4 7
+# 1
+# 2
+# 5
+# 3 6 5 4 1
+# 7
+# 1 2 3 5 6 8 9
+# 3
+# 9 8 2
+# Sample Output
+# True 
+# False
+# False
+# Explanation
+# Set A = {1 2 3 5 6}
+# Set B = {9 8 5 6 3 2 1 4 7}
+# All the elements of set A are elements of set B.
+# Hence, set A is a subset of set B.
