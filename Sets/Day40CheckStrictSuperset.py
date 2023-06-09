6adb9732b2e29d8dd05ff5c92b606ea91eef8bd1
SUGAPRIYAN P K <sugapriyan2003@gmail.com>
2023-06-09 19:12:02 +0530

    Create Day40CheckStrictSuperset.py

---
+++ Sets/Day40CheckStrictSuperset.py
@@ -0,0 +1,11 @@
+
+# Sample Input 0
+# 1 2 3 4 5 6 7 8 9 10 11 12 23 45 84 78
+# 2
+# 1 2 3 4 5
+# 100 11 12
+# Sample Output 0
+# False
+# Explanation 0
+# Set A is the strict superset of the set ([1,2,3,4,5]) but not of the set ([100,11,12]) because 100 is not in set A.
+# Hence, the output is False.
