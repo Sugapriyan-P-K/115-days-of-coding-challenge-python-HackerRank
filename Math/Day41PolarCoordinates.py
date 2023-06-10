b44f828cf6972a5550260b2dcaf4f77a35d37003
SUGAPRIYAN P K <sugapriyan2003@gmail.com>
2023-06-10 11:08:13 +0530

    Create Day41PolarCoordinates.py

---
+++ Math/Day41PolarCoordinates.py
@@ -0,0 +1,10 @@
+
+
+# Task
+# You are given a complex z. Your task is to convert it to polar coordinates.
+# Sample Input
+#   1+2j
+# Sample Output
+#  2.23606797749979 
+#  1.1071487177940904
+# Note: The output should be correct up to 3 decimal places.
