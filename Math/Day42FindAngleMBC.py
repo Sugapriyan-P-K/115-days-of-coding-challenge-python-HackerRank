b77d29a5047f69488d7fe4171217cd9eff1ba89e
SUGAPRIYAN P K <sugapriyan2003@gmail.com>
2023-06-11 12:01:45 +0530

    Create Day42FindAngleMBC.py

---
+++ Math/Day42FindAngleMBC.py
@@ -0,0 +1,12 @@
+
+
+
+# Examples:
+# If angle is 56.5000001°, then output 57°.
+# If angle is 56.5000000°, then output 57°.
+# If angle is 56.4999999°, then output 56°.
+# Sample Input
+# 10
+# 10
+# Sample Output
+# 45°
