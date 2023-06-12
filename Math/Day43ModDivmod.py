6b6a336859f199977a945b80cfd2bc3611ca935a
SUGAPRIYAN P K <sugapriyan2003@gmail.com>
2023-06-12 09:58:40 +0530

    Create Day43ModDivmod.py

---
+++ Math/Day43ModDivmod.py
@@ -0,0 +1,14 @@
+
+
+# Task
+# Read in two integers, a and b, and print three lines.
+# The first line is the integer division a//b (While using Python2 remember to import division from __future__).
+# The second line is the result of the modulo operator: a%b.
+# The third line prints the divmod of a and b.
+# Sample Input
+# 177
+# 10
+# Sample Output
+# 17
+# 7
+# (17, 7)
