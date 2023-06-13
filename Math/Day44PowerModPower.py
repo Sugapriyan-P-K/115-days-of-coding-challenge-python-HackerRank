d668e5a12e7a85a4162f4dd67380d9c312ba36b0
SUGAPRIYAN P K <sugapriyan2003@gmail.com>
2023-06-13 10:49:36 +0530

    Create Day44PowerModPower.py

---
+++ Math/Day44PowerModPower.py
@@ -0,0 +1,12 @@
+
+
+# Task
+# You are given three integers: a, b, and m. Print two lines.
+# On the first line, print the result of pow(a,b). On the second line, print the result of pow(a,b,m).
+# Sample Input
+# 3
+# 4
+# 5
+# Sample Output
+# 81
+# 1
