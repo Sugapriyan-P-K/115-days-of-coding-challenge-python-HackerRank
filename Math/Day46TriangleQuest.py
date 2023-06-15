af59038759d6b5280197244f3904c537bb4fcf38
SUGAPRIYAN P K <sugapriyan2003@gmail.com>
2023-06-15 11:19:35 +0530

    Create Day46TriangleQuest.py

---
+++ Math/Day46TriangleQuest.py
@@ -0,0 +1,18 @@
+
+
+# You are given a positive integer N. Print a numerical triangle of height N-1 like the one below:
+# 1
+# 22
+# 333
+# 4444
+# 55555
+# ......
+# Can you do it using only arithmetic operations, a single for loop and print statement?
+# Note: Using anything related to strings will give a score of 0.
+# Sample Input
+# 5
+# Sample Output
+# 1
+# 22
+# 333
+# 4444
