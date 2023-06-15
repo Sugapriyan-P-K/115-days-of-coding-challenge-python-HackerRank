4fa7dc0876d3795c986cb6d5b7bc11e6785a671a
SUGAPRIYAN P K <sugapriyan2003@gmail.com>
2023-06-15 11:20:12 +0530

    Solved Day46TriangleQuest.py

--- Math/Day46TriangleQuest.py
+++ Math/Day46TriangleQuest.py
@@ -1,4 +1,5 @@
-
+for i in range(1,int(input())): #More than 2 lines will result in 0 score. Do not leave a blank line also
+    print((10**i//9)*i)
 
 # You are given a positive integer N. Print a numerical triangle of height N-1 like the one below:
 # 1
