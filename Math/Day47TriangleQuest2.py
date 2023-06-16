36aff9a48c0eacdd5b362dcd62748b1dbff020d2
SUGAPRIYAN P K <sugapriyan2003@gmail.com>
2023-06-16 10:50:18 +0530

    Update Day47TriangleQuest2.py

--- Math/Day47TriangleQuest2.py
+++ Math/Day47TriangleQuest2.py
@@ -1,4 +1,5 @@
-
+for i in range(1,int(input())+1):
+    print((10**i//9)**2)
 # You are given a positive integer N.
 # Your task is to print a palindromic triangle of size N.
 # For example, a palindromic triangle of size 5 is:
