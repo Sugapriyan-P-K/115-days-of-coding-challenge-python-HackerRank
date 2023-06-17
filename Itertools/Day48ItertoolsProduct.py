b02bd063692129c1d9cc268f83a0a1d6c5eea7a3
SUGAPRIYAN P K <sugapriyan2003@gmail.com>
2023-06-17 11:26:04 +0530

    Create Day48ItertoolsProduct.py

---
+++ Itertools/Day48ItertoolsProduct.py
@@ -0,0 +1,10 @@
+
+
+# For example, product(A, B) returns the same as ((x,y) for x in A for y in B).
+# Task
+# You are given a two lists A and B. Your task is to compute their cartesian product A X B.
+# Sample Input
+#  1 2
+#  3 4
+# Sample Output
+#  (1, 3) (1, 4) (2, 3) (2, 4)
