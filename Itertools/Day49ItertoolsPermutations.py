b09b437c48f4911c3099039fcf733d63a0a0f7c6
SUGAPRIYAN P K <sugapriyan2003@gmail.com>
2023-06-18 13:12:06 +0530

    Create Day49ItertoolsPermutations.py

---
+++ Itertools/Day49ItertoolsPermutations.py
@@ -0,0 +1,22 @@
+
+
+# Task
+# You are given a string S.
+# Your task is to print all possible permutations of size k of the string in lexicographic sorted order.
+# Sample Input
+# HACK 2
+# Sample Output
+# AC
+# AH
+# AK
+# CA
+# CH
+# CK
+# HA
+# HC
+# HK
+# KA
+# KC
+# KH
+# Explanation
+# All possible size 2 permutations of the string "HACK" are printed in lexicographic sorted order.
