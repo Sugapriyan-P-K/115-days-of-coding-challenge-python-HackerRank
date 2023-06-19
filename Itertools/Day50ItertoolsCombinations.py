f933d87946975a3334ac97ac1436789a390d8c4f
SUGAPRIYAN P K <sugapriyan2003@gmail.com>
2023-06-19 09:50:17 +0530

    Create Day50ItertoolsCombinations.py

---
+++ Itertools/Day50ItertoolsCombinations.py
@@ -0,0 +1,18 @@
+
+
+# Task
+# You are given a string S.
+# Your task is to print all possible combinations, up to size k, of the string in lexicographic sorted order.
+# Sample Input
+# HACK 2
+# Sample Output
+# A
+# C
+# H
+# K
+# AC
+# AH
+# AK
+# CH
+# CK
+# HK
