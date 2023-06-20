a6be9f00bad7f83d202c1a9a15497aaab8ea15cd
SUGAPRIYAN P K <sugapriyan2003@gmail.com>
2023-06-20 11:47:46 +0530

    Create Day51ItertoolsCombinationsWithReplacement.py

---
+++ Itertools/Day51ItertoolsCombinationsWithReplacement.py
@@ -0,0 +1,18 @@
+
+
+# Task
+# You are given a string S.
+# Your task is to print all possible size k replacement combinations of the string in lexicographic sorted order.
+# Sample Input
+# HACK 2
+# Sample Output
+# AA
+# AC
+# AH
+# AK
+# CC
+# CH
+# CK
+# HH
+# HK
+# KK
