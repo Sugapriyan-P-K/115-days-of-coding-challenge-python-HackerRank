1eab2b511847b5403c15c75cb4fb034189361fe1
SUGAPRIYAN P K <sugapriyan2003@gmail.com>
2023-06-21 19:55:21 +0530

    Create Day52CompressTheString.py

---
+++ Itertools/Day52CompressTheString.py
@@ -0,0 +1,10 @@
+
+# You are given a string S. Suppose a character 'c' occurs consecutively X times in the string. Replace these consecutive occurrences of the character 'c' with (X,C) in the string.
+# For a better understanding of the problem, check the explanation.
+# Sample Input
+# 1222311
+# Sample Output
+# (1, 1) (3, 2) (1, 3) (2, 1)
+# Explanation
+# First, the character 1 occurs only once. It is replaced by (1,1). Then the character 2 occurs three times, and it is replaced by (3,2) and so on.
+# Also, note the single space within each compression and between the compressions.
