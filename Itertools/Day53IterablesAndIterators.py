439d955f93f7be914fcb6adbc63618e6d7ceaa8f
SUGAPRIYAN P K <sugapriyan2003@gmail.com>
2023-06-22 11:17:46 +0530

    Create Day53IterablesAndIterators.py

---
+++ Itertools/Day53IterablesAndIterators.py
@@ -0,0 +1,13 @@
+
+
+# Sample Input
+# 4 
+# a a c d
+# 2
+# Sample Output
+# 0.8333
+# Explanation
+# All possible unordered tuples of length  comprising of indices from 1 to 4 are:
+# (1,2),(1,3),(1,4),(2,3),(2,4),(3,4)
+# Out of these 6 combinations, 5 of them contain either index 1 or index 2 which are the indices that contain the letter 'a'.
+# Hence, the answer is 5/6 => 0.8333.
