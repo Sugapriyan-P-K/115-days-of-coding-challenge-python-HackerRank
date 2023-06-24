852614614454670f3f95e1825acd3da0426670b5
SUGAPRIYAN P K <sugapriyan2003@gmail.com>
2023-06-24 12:08:56 +0530

    Update Day55CollectionsCounter.py

--- Collections/Day55CollectionsCounter.py
+++ Collections/Day55CollectionsCounter.py
@@ -1,2 +1,24 @@
 
 
+# collections.Counter()
+# A counter is a container that stores elements as dictionary keys, and their counts are stored as dictionary values.
+# Sample Input
+# 10
+# 2 3 4 5 6 8 7 6 5 18
+# 6
+# 6 55
+# 6 45
+# 6 55
+# 4 40
+# 18 60
+# 10 50
+# Sample Output
+# 200
+# Explanation
+# Customer 1: Purchased size 6 shoe for $55.
+# Customer 2: Purchased size 6 shoe for $45.
+# Customer 3: Size 6 no longer available, so no purchase.
+# Customer 4: Purchased size 4 shoe for $40.
+# Customer 5: Purchased size 18 shoe for $60.
+# Customer 6: Size 10 not available, so no purchase.
+# Total money earned 55 + 45 + 40 + 60 =  $200.
