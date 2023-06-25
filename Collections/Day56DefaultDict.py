50cf3bb074d3b11b2fe0d309a4ca2e379028a2f2
SUGAPRIYAN P K <sugapriyan2003@gmail.com>
2023-06-25 11:03:37 +0530

    Create Day56DefaultDict.py

---
+++ Collections/Day56DefaultDict.py
@@ -0,0 +1,20 @@
+
+
+# Sample Input
+# STDIN   Function
+# -----   --------
+# 5 2     group A size n = 5, group B size m = 2
+# a       group A contains 'a', 'a', 'b', 'a', 'b'
+# a
+# b
+# a
+# b
+# a       group B contains 'a', 'b'
+# b
+# Sample Output
+# 1 2 4
+# 3 5
+# Explanation
+# 'a' appeared 3 times in positions 1, 2 and 4.
+# 'b' appeared 2 times in positions 3 and 5.
+# In the sample problem, if 'c' also appeared in word group B, you would print -1.
