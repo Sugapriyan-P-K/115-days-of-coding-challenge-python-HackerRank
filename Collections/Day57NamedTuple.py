94e9f2441c7de14b4109a030d9d5cc948b52ee49
SUGAPRIYAN P K <sugapriyan2003@gmail.com>
2023-06-26 08:52:01 +0530

    Create Day57NamedTuple.py

---
+++ Collections/Day57NamedTuple.py
@@ -0,0 +1,29 @@
+
+
+# Sample Input
+# TESTCASE 01
+# 5
+# ID         MARKS      NAME       CLASS     
+# 1          97         Raymond    7         
+# 2          50         Steven     4         
+# 3          91         Adrian     9         
+# 4          72         Stewart    5         
+# 5          80         Peter      6   
+# TESTCASE 02
+# 5
+# MARKS      CLASS      NAME       ID        
+# 92         2          Calum      1         
+# 82         5          Scott      2         
+# 94         2          Jason      3         
+# 55         8          Glenn      4         
+# 82         2          Fergus     5
+# Sample Output
+# TESTCASE 01
+# 78.00
+# TESTCASE 02
+# 81.00
+# Explanation
+# TESTCASE 01
+# Average = (97 + 50 + 91 + 72 + 80) / 5
+#  Can you solve this challenge in 4 lines of code or less?
+# NOTE: There is no penalty for solutions that are correct but have more than 4 lines.
