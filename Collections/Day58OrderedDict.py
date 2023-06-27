2cffc994d33b211e5817424bbf7a0bffadad80c8
SUGAPRIYAN P K <sugapriyan2003@gmail.com>
2023-06-27 14:11:09 +0530

    Create Day58OrderedDict.py

---
+++ Collections/Day58OrderedDict.py
@@ -0,0 +1,27 @@
+
+
+# Sample Input
+# 9
+# BANANA FRIES 12
+# POTATO CHIPS 30
+# APPLE JUICE 10
+# CANDY 5
+# APPLE JUICE 10
+# CANDY 5
+# CANDY 5
+# CANDY 5
+# POTATO CHIPS 30
+# Sample Output
+# BANANA FRIES 12
+# POTATO CHIPS 60
+# APPLE JUICE 20
+# CANDY 20
+# Explanation
+# BANANA FRIES: Quantity bought: 1, Price:12 
+# Net Price: 12
+# POTATO CHIPS: Quantity bought: 2, Price: 30
+# Net Price: 60
+# APPLE JUICE: Quantity bought: 2, Price: 10
+# Net Price: 20
+# CANDY: Quantity bought: 4, Price: 5
+# Net Price:20
