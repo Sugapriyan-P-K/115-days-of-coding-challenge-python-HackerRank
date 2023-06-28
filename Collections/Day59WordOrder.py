55d4ff525f93a0f80e6f1748109edd95c8d7b29d
SUGAPRIYAN P K <sugapriyan2003@gmail.com>
2023-06-28 11:28:08 +0530

    Create Day59WordOrder.py

---
+++ Collections/Day59WordOrder.py
@@ -0,0 +1,17 @@
+
+
+# You are given n words. Some words may repeat. For each word, output its number of occurrences. The output order should correspond with the input order of appearance of the word.
+# See the sample input/output for clarification.
+# Note: Each input line ends with a "\n" character.
+# Sample Input
+# 4
+# bcdef
+# abcdefg
+# bcde
+# bcdef
+# Sample Output
+# 3
+# 2 1 1
+# Explanation
+# There are 3 distinct words. Here, "bcdef" appears twice in the input at the first and last positions. The other words appear once each. 
+# The order of the first appearances are "bcdef", "abcdefg" and "bcde" which corresponds to the output.
