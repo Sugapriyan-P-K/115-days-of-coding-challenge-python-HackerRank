51d4a828047688d4d0cf79308a18f58a7219ce18
SUGAPRIYAN P K <sugapriyan2003@gmail.com>
2023-06-29 10:47:25 +0530

    Create Day60Deque.py

---
+++ Collections/Day60Deque.py
@@ -0,0 +1,14 @@
+
+
+# Task
+# Perform append, pop, popleft and appendleft methods on an empty deque d.
+# Sample Input
+# 6
+# append 1
+# append 2
+# append 3
+# appendleft 4
+# pop
+# popleft
+# Sample Output
+# 1 2
