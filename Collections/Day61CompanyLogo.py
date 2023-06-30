7c455558c6b02b9fcbaa5f3a3f5e73e609052403
SUGAPRIYAN P K <sugapriyan2003@gmail.com>
2023-06-30 22:36:29 +0530

    Create Day61CompanyLogo.py

---
+++ Collections/Day61CompanyLogo.py
@@ -0,0 +1,19 @@
+
+
+# Given a string , which is the company name in lowercase letters, your task is to find the top three most common characters in the string.
+# Print the three most common characters along with their occurrence count.
+# Sort in descending order of occurrence count.
+# If the occurrence count is the same, sort the characters in alphabetical order.
+# For example, according to the conditions described above,
+# GOOGLE would have it's logo with the letters G,O,E.
+# Sample Input 0
+# aabbbccde
+# Sample Output 0
+# b 3
+# a 2
+# c 2
+# Explanation 0
+# aabbbccde
+# Here, b occurs 3 times. It is printed first.
+# Both a and c occur 2 times. So, a is printed in the second line and c in the third line because a comes before c in the alphabet.
+# Note: The string S has at least 3 distinct characters.
