cf7af02a8528806cc5ce70dde044cd815517f5ab
SUGAPRIYAN P K <sugapriyan2003@gmail.com>
2023-07-02 12:58:22 +0530

    Update CalendarModule.py

--- DateAndTime/CalendarModule.py
+++ DateAndTime/CalendarModule.py
@@ -1,4 +1,7 @@
-
+from datetime import datetime
+date = input()
+date = datetime.strptime(date, "%m %d %Y")
+print(date.strftime("%A").upper())
 
 # Task
 # You are given a date. Your task is to find what the day is on that date.
