47d97912f0d48650be73b45785b2e89a05503870
SUGAPRIYAN P K <sugapriyan2003@gmail.com>
2023-07-02 12:57:41 +0530

    Update CalendarModule.py

--- DateAndTime/CalendarModule.py
+++ DateAndTime/CalendarModule.py
@@ -1,1 +1,10 @@
 
+
+# Task
+# You are given a date. Your task is to find what the day is on that date.
+# Sample Input
+# 08 05 2015
+# Sample Output
+# WEDNESDAY
+# Explanation
+# The day on August 5th 2015 was WEDNESDAY.
