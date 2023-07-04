26db9b92e81759adef34f36cecd1095125593c50
SUGAPRIYAN P K <sugapriyan2003@gmail.com>
2023-07-04 12:21:08 +0530

    Update Exceptionss.py

--- ErrorsAndExceptions/Exceptionss.py
+++ ErrorsAndExceptions/Exceptionss.py
@@ -1,1 +1,18 @@
 
+
+# Task
+# You are given two values a and b.
+# Perform integer division and print a/b.
+# Sample Input
+
+# 3
+# 1 0
+# 2 $
+# 3 1
+# Sample Output
+
+# Error Code: integer division or modulo by zero
+# Error Code: invalid literal for int() with base 10: '$'
+# 3
+# Note:
+# For integer division in Python 3 use //.
