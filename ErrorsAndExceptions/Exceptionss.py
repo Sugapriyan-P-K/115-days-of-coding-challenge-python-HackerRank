23d885b5084cdcf5de73dbc9909c663b2bc3f632
SUGAPRIYAN P K <sugapriyan2003@gmail.com>
2023-07-04 12:21:42 +0530

    Update Exceptionss.py

--- ErrorsAndExceptions/Exceptionss.py
+++ ErrorsAndExceptions/Exceptionss.py
@@ -1,16 +1,19 @@
-
+for i in range(int(input())):
+    try:
+        a,b = map(int, input().split())
+        print(a//b)
+    except Exception as e:
+        print('Error Code:', e)
 
 # Task
 # You are given two values a and b.
 # Perform integer division and print a/b.
 # Sample Input
-
 # 3
 # 1 0
 # 2 $
 # 3 1
 # Sample Output
-
 # Error Code: integer division or modulo by zero
 # Error Code: invalid literal for int() with base 10: '$'
 # 3
