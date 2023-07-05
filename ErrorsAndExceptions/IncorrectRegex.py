cca240a1f98eb47593baeaecdcdf008a1361f077
SUGAPRIYAN P K <sugapriyan2003@gmail.com>
2023-07-05 08:42:02 +0530

    Create IncorrectRegex.py

---
+++ ErrorsAndExceptions/IncorrectRegex.py
@@ -0,0 +1,14 @@
+
+
+# You are given a string S.
+# Your task is to find out whether S is a valid regex or not.
+# Sample Input
+# 2
+# .*\+
+# .*+
+# Sample Output
+# True
+# False
+# Explanation
+# .*\+ : Valid regex.
+# .*+: Has the error multiple repeat. Hence, it is invalid.
