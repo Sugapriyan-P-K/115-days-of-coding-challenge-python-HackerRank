5a9cd44f6652621b95a22777a8a48523c923b6c1
SUGAPRIYAN P K <sugapriyan2003@gmail.com>
2023-07-06 13:39:41 +0530

    Update DealingWithComplexNumber.py

--- Classes/DealingWithComplexNumber.py
+++ Classes/DealingWithComplexNumber.py
@@ -1,2 +1,18 @@
 
 
+# Sample Input
+# 2 1
+# 5 6
+# Sample Output
+# 7.00+7.00i
+# -3.00-5.00i
+# 4.00+17.00i
+# 0.26-0.11i
+# 2.24+0.00i
+# 7.81+0.00i
+# Concept
+# Python is a fully object-oriented language like C++, Java, etc. For reading about classes.
+# Methods with a double underscore before and after their name are considered as built-in methods. They are used by interpreters and are generally used in the implementation of overloaded operators or other built-in functionality.
+# __add__-> Can be overloaded for + operation
+# __sub__ -> Can be overloaded for - operation
+# __mul__ -> Can be overloaded for * operation
