95c25b8160ffbee2f21faca6666eb6893b8659ca
SUGAPRIYAN P K <sugapriyan2003@gmail.com>
2023-07-07 08:42:21 +0530

    Create FindTheTorsionalAngle.py

---
+++ Classes/FindTheTorsionalAngle.py
@@ -0,0 +1,13 @@
+
+
+# Input Format
+# One line of input containing the space separated floating number values of the X, Y and Z coordinates of a point.
+# Output Format
+# Output the angle correct up to two decimal places.
+# Sample Input
+# 0 4 5
+# 1 7 6
+# 0 5 9
+# 1 7 2
+# Sample Output
+# 8.19
