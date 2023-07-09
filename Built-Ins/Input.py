9c592bda53e97e91ac59ddfc328c0ea2a978e696
SUGAPRIYAN P K <sugapriyan2003@gmail.com>
2023-07-09 13:08:49 +0530

    Create Input.py

---
+++ Built-Ins/Input.py
@@ -0,0 +1,13 @@
+
+
+# Task
+# You are given a polynomial P of a single indeterminate (or variable), x.
+# You are also given the values of x and k. Your task is to verify if P(x) = k.
+# Sample Input
+# 1 4
+# x**3 + x**2 + x + 1
+# Sample Output
+# True
+# Explanation
+# P(1) = 1**3 + 1**2 + 1 + 1 = 4 = k (where k = 4)
+# Hence, the output is True.
