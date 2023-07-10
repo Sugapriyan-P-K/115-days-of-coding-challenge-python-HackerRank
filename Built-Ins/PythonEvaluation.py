b3ad0fa158407ec4c88e1a2f5640ab13c7483974
SUGAPRIYAN P K <sugapriyan2003@gmail.com>
2023-07-10 18:59:46 +0530

    Create PythonEvaluation.py

---
+++ Built-Ins/PythonEvaluation.py
@@ -0,0 +1,11 @@
+
+
+# Task
+# You are given an expression in a line. Read that line as a string variable, such as var, and print the result using eval(var).
+# NOTE: Python2 users, please import from __future__ import print_function.
+# Constraint
+# Input string is less than 100 characters.
+# Sample Input
+# print(2 + 3)
+# Sample Output
+# 5
