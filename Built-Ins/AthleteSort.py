11a39bb4e7efe5b98507ee646aa1d8c80dc9212a
SUGAPRIYAN P K <sugapriyan2003@gmail.com>
2023-07-11 20:50:11 +0530

    Create AthleteSort.py

---
+++ Built-Ins/AthleteSort.py
@@ -0,0 +1,24 @@
+
+
+# Input Format
+# The first line contains N and M separated by a space.
+# The next N lines each contain M elements.
+# The last line contains K.
+# Output Format
+# Print the N lines of the sorted table. Each line should contain the space separated elements. Check the sample below for clarity.
+# Sample Input 0
+# 5 3
+# 10 2 5
+# 7 1 0
+# 9 9 9
+# 1 23 12
+# 6 5 9
+# 1
+# Sample Output 0
+# 7 1 0
+# 10 2 5
+# 6 5 9
+# 9 9 9
+# 1 23 12
+# Explanation 0
+# The details are sorted based on the second attribute, since K is zero-indexed.
