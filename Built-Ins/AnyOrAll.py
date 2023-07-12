f5f7bfb9e6bc411675e8ae7d0f6cb9007841a667
SUGAPRIYAN P K <sugapriyan2003@gmail.com>
2023-07-12 09:19:56 +0530

    Create AnyOrAll.py

---
+++ Built-Ins/AnyOrAll.py
@@ -0,0 +1,15 @@
+
+
+# Task
+# You are given a space separated list of integers. If all the integers are positive, then you need to check if any integer is a palindromic integer.
+# Sample Input
+# 5
+# 12 9 61 5 14 
+# Sample Output
+# True
+# Explanation
+# Condition 1: All the integers in the list are positive.
+# Condition 2: 5 is a palindromic integer.
+# Hence, the output is True.
+# Can you solve this challenge in 3 lines of code or less?
+# There is no penalty for solutions that are correct but have more than 3 lines.
