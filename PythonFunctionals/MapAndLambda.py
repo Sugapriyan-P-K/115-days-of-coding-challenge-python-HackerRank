d75c7a952c35c3f1a8e49dc2e1ebbde4563c09d3
SUGAPRIYAN P K <sugapriyan2003@gmail.com>
2023-07-14 10:37:44 +0530

    Update MapAndLambda.py

--- PythonFunctionals/MapAndLambda.py
+++ PythonFunctionals/MapAndLambda.py
@@ -1,4 +1,22 @@
-
+cube = lambda x: x*x*x # complete the lambda function 
+def fibonacci(n):
+    n1,n2 = 0,1
+    l = [0,1]
+    if n==0:
+        return []
+    elif n==1:
+        return [n1]
+    else:
+        for i in range(2,n):
+            n3 = n1 + n2
+            n1 = n2
+            n2 = n3
+            l.append(n2)
+        return l
+    # return a list of fibonacci numbers
+if __name__ == '__main__':
+    n = int(input())
+    print(list(map(cube, fibonacci(n))))
 
 
 # Sample Input
