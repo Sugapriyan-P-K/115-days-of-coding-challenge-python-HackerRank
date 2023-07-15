827460b0fb53fa9d534776ef4b0114e9453c8f0d
SUGAPRIYAN P K <sugapriyan2003@gmail.com>
2023-07-15 11:37:10 +0530

    Create ValidatingEmailAddress.py

---
+++ PythonFunctionals/ValidatingEmailAddress.py
@@ -0,0 +1,16 @@
+
+
+# You are given an integer N followed by N email addresses. Your task is to print a list containing only valid email addresses in lexicographical order.
+# Valid email addresses must follow these rules:
+# It must have the username@websitename.extension format type.
+# The username can only contain letters, digits, dashes and underscores [a-z], [A-Z], [0-9], [_-].
+# The website name can only have letters and digits [a-z], [A-Z], [0-9].
+# The extension can only contain letters [a-z], [A-Z].
+# The maximum length of the extension is 3.
+# Sample Input
+# 3
+# lara@hackerrank.com
+# brian-23@hackerrank.com
+# britts_54@hackerrank.com
+# Sample Output
+# ['brian-23@hackerrank.com', 'britts_54@hackerrank.com', 'lara@hackerrank.com']
