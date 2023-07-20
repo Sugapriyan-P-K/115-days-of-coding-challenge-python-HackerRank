d6c449c96d51d62ab18f0ba1ddbe133d7812c4a5
SUGAPRIYAN P K <sugapriyan2003@gmail.com>
2023-07-20 19:41:26 +0530

    Update NameDirectory.py

--- ClosuresAndDecorators/NameDirectory.py
+++ ClosuresAndDecorators/NameDirectory.py
@@ -1,4 +1,19 @@
+import operator
+def person_lister(f):
+    def inner(people):
+        l=[]
+        people = sorted(people,key=lambda x:int(x[2]),reverse = False)
+        for p in people:
+            l.append(f(p))
+        return l
+    return inner
+@person_lister
+def name_format(person):
+    return ("Mr. " if person[3] == "M" else "Ms. ") + person[0] + " " + person[1]
 
+if __name__ == '__main__':
+    people = [input().split() for i in range(int(input()))]
+    print(*name_format(people), sep='\n')
 
 # Let's use decorators to build a name directory! You are given some information about N people. Each person has a first name, last name, age and sex. 
 # Print their names in a specific format sorted by their age in ascending order i.e. the youngest person's name should be printed first. For two people of the same age, 
