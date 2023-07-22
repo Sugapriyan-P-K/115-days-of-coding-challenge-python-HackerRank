1fe8d5a66dc5e99651b49393e78403ed85509d81
SUGAPRIYAN P K <sugapriyan2003@gmail.com>
2023-07-22 11:34:53 +0530

    Update DefaultArguments.py

--- Debugging/DefaultArguments.py
+++ Debugging/DefaultArguments.py
@@ -1,3 +1,36 @@
+class EvenStream(object):
+    def __init__(self):
+        self.current = 0
+
+    def get_next(self):
+        to_return = self.current
+        self.current += 2
+        return to_return
+
+class OddStream(object):
+    def __init__(self):
+        self.current = 1
+
+    def get_next(self):
+        to_return = self.current
+        self.current += 2
+        return to_return
+
+def print_from_stream(n, stream=None):
+    if stream==None: # add this condition
+        stream = EvenStream()
+    for _ in range(n):
+        print(stream.get_next())
+        
+
+queries = int(input())
+for _ in range(queries):
+    stream_name, n = input().split()
+    n = int(n)
+    if stream_name == "even":
+        print_from_stream(n)
+    else:
+        print_from_stream(n, OddStream())
 
 
 # Debug the given function print_from_stream using the default value of one of its arguments.
