1a287348863d1858be1de307d44288ee05c7f6a4
SUGAPRIYAN P K <sugapriyan2003@gmail.com>
2023-07-24 10:29:06 +0530

    Create ReSplit.py

---
+++ RegexAndParsing/ReSplit.py
@@ -0,0 +1,12 @@
+
+
+# You are given a string s consisting only of digits 0-9, commas ,, and dots .
+# Your task is to complete the regex_pattern defined below, which will be used to re.split() all of the , and . symbols in s.
+# It’s guaranteed that every comma and every dot in s is preceeded and followed by a digit.
+# Sample Input 0
+# 100,000,000.000
+# Sample Output 0
+# 100
+# 000
+# 000
+# 000
