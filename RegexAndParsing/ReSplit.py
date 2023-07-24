c0f04afe7e87b8e1aaabea6b35deeea47e173f17
SUGAPRIYAN P K <sugapriyan2003@gmail.com>
2023-07-24 10:30:16 +0530

    Update ReSplit.py

--- RegexAndParsing/ReSplit.py
+++ RegexAndParsing/ReSplit.py
@@ -1,4 +1,6 @@
-
+regex_pattern = r"[,.]"	
+import re
+print("\n".join(re.split(regex_pattern, input())))
 
 # You are given a string s consisting only of digits 0-9, commas ,, and dots .
 # Your task is to complete the regex_pattern defined below, which will be used to re.split() all of the , and . symbols in s.
