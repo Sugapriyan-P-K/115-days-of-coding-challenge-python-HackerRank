b0b2d4b86e1a62cf302fc0e9ca7d291d8d3e70fe
SUGAPRIYAN P K <sugapriyan2003@gmail.com>
2023-07-25 10:27:32 +0530

    Create Groupss.py

---
+++ RegexAndParsing/Groupss.py
@@ -0,0 +1,12 @@
+
+
+# Task
+# You are given a string S.
+# Your task is to find the first occurrence of an alphanumeric character in S (read from left to right) that has consecutive repetitions.
+# Sample Input
+# ..12345678910111213141516171820212223
+# Sample Output
+# 1
+# Explanation
+# .. is the first repeating character, but it is not alphanumeric.
+# 1 is the first (from left to right) alphanumeric repeating character of the string in the substring 111.
