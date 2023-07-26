6f48cda00e0fa1e0014d3229eda64dee18ba0214
SUGAPRIYAN P K <sugapriyan2003@gmail.com>
2023-07-26 12:51:06 +0530

    Create ReFinds.py

---
+++ RegexAndParsing/ReFinds.py
@@ -0,0 +1,21 @@
+
+
+# Task
+# You are given a string S. It consists of alphanumeric characters, spaces and symbols(+,-).
+# Your task is to find all the substrings of S that contains 2 or more vowels.
+# Also, these substrings must lie in between 2 consonants and should contain vowels only.
+# Note :
+# Vowels are defined as: AEIOU and aeiou.
+# Consonants are defined as: QWRTYPSDFGHJKLZXCVBNM and qwrtypsdfghjklzxcvbnm.
+# Sample Input
+# rabcdeefgyYhFjkIoomnpOeorteeeeet
+# Sample Output
+# ee
+# Ioo
+# Oeo
+# eeeee
+# Explanation
+# ee is located between consonant d and f.
+# Ioo is located between consonant k and m.
+# Oeo is located between consonant p and r.
+# eeeee is located between consonant t and t.
