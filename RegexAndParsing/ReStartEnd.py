353a71eadef4b50c6477660aa2131d1a70bd6e7a
SUGAPRIYAN P K <sugapriyan2003@gmail.com>
2023-07-27 09:52:38 +0530

    Create ReStartEnd.py

---
+++ RegexAndParsing/ReStartEnd.py
@@ -0,0 +1,12 @@
+
+
+# Task
+# You are given a string S.
+# Your task is to find the indices of the start and end of string k in S.
+# Sample Input
+# aaadaa
+# aa
+# Sample Output
+# (0, 1)  
+# (1, 2)
+# (4, 5)
