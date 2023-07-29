50bf9d1fb859d38698e929f0ff9e2f530d8c1400
SUGAPRIYAN P K <sugapriyan2003@gmail.com>
2023-07-29 15:42:46 +0530

    Create ValidRomanNumerals.py

---
+++ RegexAndParsing/ValidRomanNumerals.py
@@ -0,0 +1,9 @@
+
+
+# You are given a string, and you have to validate whether it's a valid Roman numeral. If it is valid, print True. Otherwise, print False. 
+# Try to create a regular expression for a valid Roman numeral.
+# Sample Input
+# CDXXI
+# Sample Output
+# True
+# https://www.britannica.com/topic/Roman-numeral - to learn about Roman Numerals
