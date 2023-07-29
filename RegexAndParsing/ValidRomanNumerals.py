8690df06af0c24ac8e3172bed86bce8d860b2933
SUGAPRIYAN P K <sugapriyan2003@gmail.com>
2023-07-29 15:43:31 +0530

    Update ValidRomanNumerals.py

--- RegexAndParsing/ValidRomanNumerals.py
+++ RegexAndParsing/ValidRomanNumerals.py
@@ -1,4 +1,6 @@
-
+regex_pattern = r"^M{0,3}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})$"
+import re
+print(str(bool(re.match(regex_pattern, input()))))
 
 # You are given a string, and you have to validate whether it's a valid Roman numeral. If it is valid, print True. Otherwise, print False. 
 # Try to create a regular expression for a valid Roman numeral.
