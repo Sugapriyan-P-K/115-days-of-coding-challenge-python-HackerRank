10447f8b1111ad1d086bbeb2dd8ae2719396658d
SUGAPRIYAN P K <sugapriyan2003@gmail.com>
2023-07-30 10:11:00 +0530

    Create ValidPhoneNumbers.py

---
+++ RegexAndParsing/ValidPhoneNumbers.py
@@ -0,0 +1,11 @@
+
+
+# Let's dive into the interesting topic of regular expressions! You are given some input, and you are required to check whether they are valid mobile numbers.
+# A valid mobile number is a ten digit number starting with a 7, 8 or 9.
+# Sample Input
+# 2
+# 9587456281
+# 1252478965
+# Sample Output
+# YES
+# NO
