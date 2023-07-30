fa8c6f9b085920fba7fac0c90d9a3eed896f5046
SUGAPRIYAN P K <sugapriyan2003@gmail.com>
2023-07-30 10:11:37 +0530

    Update ValidPhoneNumbers.py

--- RegexAndParsing/ValidPhoneNumbers.py
+++ RegexAndParsing/ValidPhoneNumbers.py
@@ -1,4 +1,9 @@
-
+import re
+for i in range(int(input())):
+    if bool(re.search(r'^[7-9](\d{9})$', input())):
+        print('YES')
+    else:
+        print('NO')
 
 # Let's dive into the interesting topic of regular expressions! You are given some input, and you are required to check whether they are valid mobile numbers.
 # A valid mobile number is a ten digit number starting with a 7, 8 or 9.
