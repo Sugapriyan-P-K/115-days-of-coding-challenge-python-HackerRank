28faeabcf672c7b5f6e31225de74a07a0d0c85fa
SUGAPRIYAN P K <sugapriyan2003@gmail.com>
2023-08-01 19:38:17 +0530

    Create HexColorCode.py

---
+++ RegexAndParsing/HexColorCode.py
@@ -0,0 +1,38 @@
+
+
+# CSS colors are defined using a hexadecimal (HEX) notation for the combination of Red, Green, and Blue color values (RGB).
+# Specifications of HEX Color Code
+# It must start with a '#' symbol.
+# It can have 3 or 6 digits.
+# Each digit is in the range of 0 to F. (1,2,3,4,5,6,7,8,9,0,A,B,C,D,E and F).
+# A-F letters can be lower case. (a,b,c,d,e and f are also valid digits).
+# Sample Input
+# 11
+# #BED
+# {
+#     color: #FfFdF8; background-color:#aef;
+#     font-size: 123px;
+#     background: -webkit-linear-gradient(top, #f9f9f9, #fff);
+# }
+# #Cab
+# {
+#     background-color: #ABC;
+#     border: 2px dashed #fff;
+# }   
+# Sample Output
+# #FfFdF8
+# #aef
+# #f9f9f9
+# #fff
+# #ABC
+# #fff
+# Explanation
+# #BED and #Cab satisfy the Hex Color Code criteria, but they are used as selectors and not as color codes in the given CSS.
+# Hence, the valid color codes are:
+# #FfFdF8
+# #aef
+# #f9f9f9
+# #fff
+# #ABC
+# #fff
+# Note: There are no comments ( // or /* */) in CSS Code.
