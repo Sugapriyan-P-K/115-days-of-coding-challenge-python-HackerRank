7bd685356fa0474945e1066ad45910ac392b309f
SUGAPRIYAN P K <sugapriyan2003@gmail.com>
2023-08-03 20:04:18 +0530

    Update HTMLParser2.py

--- RegexAndParsing/HTMLParser2.py
+++ RegexAndParsing/HTMLParser2.py
@@ -1,3 +1,26 @@
+from html.parser import HTMLParser
+class MyHTMLParser(HTMLParser):
+    def handle_data(self, data):
+        if data.strip():
+            print('>>> Data')
+            print(data)
+    def handle_comment(self, data):
+        noOfLine = len(data.split('\n'))
+        if noOfLine > 1:
+            print('>>> Multi-line Comment')
+        else:
+            print('>>> Single-line Comment')
+        print(data)
+
+  
+html = ""       
+for i in range(int(input())):
+    html += input().rstrip()
+    html += '\n'
+    
+parser = MyHTMLParser()
+parser.feed(html)
+parser.close()
 
 
 # Task
