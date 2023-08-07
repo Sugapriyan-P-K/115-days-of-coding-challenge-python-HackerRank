3422af8e79d31d9c1aec2f21b4dccecf48ef3d2e
SUGAPRIYAN P K <sugapriyan2003@gmail.com>
2023-08-07 19:04:58 +0530

    Update ValidPostalCodes.py

--- RegexAndParsing/ValidPostalCodes.py
+++ RegexAndParsing/ValidPostalCodes.py
@@ -1,4 +1,9 @@
-
+regex_integer_in_range = r"^[1-9][0-9]{5}$"
+regex_alternating_repetitive_digit_pair = r"(?=(\d)\d\1)"
+import re
+P = input()
+print (bool(re.match(regex_integer_in_range, P)) 
+and len(re.findall(regex_alternating_repetitive_digit_pair, P)) < 2)
 
 # A valid postal code  have to fullfil both below requirements:
 # 1. P must be a number in the range from 100000 to 999999 inclusive.
