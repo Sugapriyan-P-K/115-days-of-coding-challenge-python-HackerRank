16d9551138bae5ce6fd6a176f10cf0f8ac69d172
SUGAPRIYAN P K <sugapriyan2003@gmail.com>
2023-08-08 09:46:52 +0530

    Update MatrixScript.py

--- RegexAndParsing/MatrixScript.py
+++ RegexAndParsing/MatrixScript.py
@@ -1,3 +1,25 @@
+import math
+import os
+import random
+import re
+import sys
+
+first_multiple_input = input().rstrip().split()
+
+n = int(first_multiple_input[0])
+
+m = int(first_multiple_input[1])
+# from here you have to code it
+matrix = []
+
+for _ in range(n):
+    matrix_item = input()
+    matrix.append(matrix_item)
+resMatrix = ''
+for i in range(m):
+    for j in range(n):
+        resMatrix += matrix[j][i]
+print(re.sub(r'(?<=\w)\W+(?=\w)', ' ', resMatrix))
 
 
 # Neo has a complex matrix script. The matrix script is a N X M grid of strings. It consists of alphanumeric characters, spaces and symbols (!,@,#,$,%,&).
