eadd62b3d2b5b4c40560aa6d4973d8bea1f86e22
SUGAPRIYAN P K <sugapriyan2003@gmail.com>
2023-08-09 14:54:59 +0530

    Update Arrays.py

--- Numpy/Arrays.py
+++ Numpy/Arrays.py
@@ -1,1 +1,9 @@
 
+
+# Task
+# You are given a space separated list of numbers.
+# Your task is to print a reversed NumPy array with the element type float.
+# Sample Input
+# 1 2 3 4 -8 -10
+# Sample Output
+# [-10.  -8.   4.   3.   2.   1.]
