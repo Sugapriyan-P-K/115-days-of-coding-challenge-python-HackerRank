9b30393e5df2dd9d140fbaa0e09812225314acd7
SUGAPRIYAN P K <sugapriyan2003@gmail.com>
2023-08-10 20:43:11 +0530

    Create shapeReshape.py

---
+++ Numpy/shapeReshape.py
@@ -0,0 +1,10 @@
+
+
+# Task
+# You are given a space separated list of nine integers. Your task is to convert this list into a 3 X 3 NumPy array.
+# Sample Input
+# 1 2 3 4 5 6 7 8 9
+# Sample Output
+# [[1 2 3]
+#  [4 5 6]
+#  [7 8 9]]
