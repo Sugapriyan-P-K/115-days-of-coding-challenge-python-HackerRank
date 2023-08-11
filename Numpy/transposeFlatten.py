05b7165f461a2bef3b10271da2b3c5ecabef76e9
SUGAPRIYAN P K <sugapriyan2003@gmail.com>
2023-08-11 20:29:14 +0530

    Update transposeFlatten.py

--- Numpy/transposeFlatten.py
+++ Numpy/transposeFlatten.py
@@ -1,4 +1,8 @@
-
+import numpy
+n,m = map(int, input().split())
+arr = numpy.array([input().strip().split() for _ in range(n)], int)
+print(arr.transpose())
+print(arr.flatten())
 
 # Task
 # You are given a N X M integer array matrix with space separated elements (N = rows and M = columns).
