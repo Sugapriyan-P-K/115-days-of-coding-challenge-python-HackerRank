2f2f8b1ddd8e5dc642a9ec7e6439afd41ca87b06
SUGAPRIYAN P K <sugapriyan2003@gmail.com>
2023-08-11 20:28:27 +0530

    Create transposeFlatten.py

---
+++ Numpy/transposeFlatten.py
@@ -0,0 +1,13 @@
+
+
+# Task
+# You are given a N X M integer array matrix with space separated elements (N = rows and M = columns).
+# Your task is to print the transpose and flatten results.
+# Sample Input
+# 2 2
+# 1 2
+# 3 4
+# Sample Output
+# [[1 3]
+#  [2 4]]
+# [1 2 3 4]
