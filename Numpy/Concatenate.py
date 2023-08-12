0ab92f514c1a4a546f88c9f768424a6fb7006540
SUGAPRIYAN P K <sugapriyan2003@gmail.com>
2023-08-12 11:22:14 +0530

    Create Concatenate.py

---
+++ Numpy/Concatenate.py
@@ -0,0 +1,21 @@
+
+
+# Task
+# You are given two integer arrays of size N X P and M X P (N & M are rows, and P is the column). Your task is to concatenate the arrays along axis 0.
+# Sample Input
+# 4 3 2
+# 1 2
+# 1 2 
+# 1 2
+# 1 2
+# 3 4
+# 3 4
+# 3 4 
+# Sample Output
+# [[1 2]
+#  [1 2]
+#  [1 2]
+#  [1 2]
+#  [3 4]
+#  [3 4]
+#  [3 4]] 
