b954e0f650c8f716ba207e952e94af2e9b9ce697
SUGAPRIYAN P K <sugapriyan2003@gmail.com>
2023-08-13 11:48:26 +0530

    Create ZeroesAndOnes.py

---
+++ Numpy/ZeroesAndOnes.py
@@ -0,0 +1,32 @@
+
+
+# Task
+# You are given the shape of the array in the form of space-separated integers, each integer representing the size of different dimensions,
+# your task is to print an array of the given shape and integer type using the tools numpy.zeros and numpy.ones.
+# Sample Input 0
+# 3 3 3
+# Sample Output 0
+# [[[0 0 0]
+#   [0 0 0]
+#   [0 0 0]]
+
+#  [[0 0 0]
+#   [0 0 0]
+#   [0 0 0]]
+
+#  [[0 0 0]
+#   [0 0 0]
+#   [0 0 0]]]
+# [[[1 1 1]
+#   [1 1 1]
+#   [1 1 1]]
+
+#  [[1 1 1]
+#   [1 1 1]
+#   [1 1 1]]
+
+#  [[1 1 1]
+#   [1 1 1]
+#   [1 1 1]]]
+# Explanation 0
+# Print the array built using numpy.zeros and numpy.ones tools and you get the result as shown.
