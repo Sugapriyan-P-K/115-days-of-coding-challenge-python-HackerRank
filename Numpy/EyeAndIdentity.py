e2bd1bf5ab2c60fa9755839607f825eceabfd426
SUGAPRIYAN P K <sugapriyan2003@gmail.com>
2023-08-14 19:08:36 +0530

    Update EyeAndIdentity.py

--- Numpy/EyeAndIdentity.py
+++ Numpy/EyeAndIdentity.py
@@ -1,4 +1,8 @@
-
+import numpy
+a,b = map(int, input().split())
+numpy.set_printoptions(legacy='1.13')
+print(numpy.eye(a,b,k=0))
+#print(numpy.identity(a,b))
 
 # Task
 # Your task is to print an array of size N X M with its main diagonal elements as 1's and 0's everywhere else.
