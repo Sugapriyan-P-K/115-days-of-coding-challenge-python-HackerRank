b076e700ab91a54ba0eaa2aaf3e1c575dd6a4afb
SUGAPRIYAN P K <sugapriyan2003@gmail.com>
2023-08-14 19:07:29 +0530

    Create EyeAndIdentity.py

---
+++ Numpy/EyeAndIdentity.py
@@ -0,0 +1,10 @@
+
+
+# Task
+# Your task is to print an array of size N X M with its main diagonal elements as 1's and 0's everywhere else.
+# Sample Input
+# 3 3
+# Sample Output
+# [[ 1.  0.  0.]
+#  [ 0.  1.  0.]
+#  [ 0.  0.  1.]]
