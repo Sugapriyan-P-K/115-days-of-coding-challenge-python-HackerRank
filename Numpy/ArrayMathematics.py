f0cf6d0bb7090b21b02ae052678d7b99bab06a5d
SUGAPRIYAN P K <sugapriyan2003@gmail.com>
2023-08-15 10:38:14 +0530

    Create ArrayMathematics.py

---
+++ Numpy/ArrayMathematics.py
@@ -0,0 +1,22 @@
+
+
+# Task
+# You are given two integer arrays, A and B of dimensions N X M.
+# Your task is to perform the following operations:
+# Add (A + B)
+# Subtract (A - B)
+# Multiply (A * B)
+# Integer Division (A / B)
+# Mod (A % B)
+# Power (A ** B)
+# Sample Input
+# 1 4
+# 1 2 3 4
+# 5 6 7 8
+# Sample Output
+# [[ 6  8 10 12]]
+# [[-4 -4 -4 -4]]
+# [[ 5 12 21 32]]
+# [[0 0 0 0]]
+# [[1 2 3 4]]
+# [[    1    64  2187 65536]] 
