fe7137ae7f0c1e4dfa9c2352f9c63e3378cdb00b
SUGAPRIYAN P K <sugapriyan2003@gmail.com>
2023-08-16 10:58:50 +0530

    Create FloorCeilRint.py

---
+++ Numpy/FloorCeilRint.py
@@ -0,0 +1,9 @@
+
+# Task
+# You are given a 1-D array, A. Your task is to print the floor, ceil and rint of all the elements of A.
+# Sample Input
+# 1.1 2.2 3.3 4.4 5.5 6.6 7.7 8.8 9.9
+# Sample Output
+# [ 1.  2.  3.  4.  5.  6.  7.  8.  9.]
+# [  2.   3.   4.   5.   6.   7.   8.   9.  10.]
+# [  1.   2.   3.   4.   6.   7.   8.   9.  10.]
