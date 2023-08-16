5e52bb02815fdeac98e0fae5abd4384fbb76848a
SUGAPRIYAN P K <sugapriyan2003@gmail.com>
2023-08-16 10:59:47 +0530

    Update FloorCeilRint.py

--- Numpy/FloorCeilRint.py
+++ Numpy/FloorCeilRint.py
@@ -1,3 +1,9 @@
+import numpy
+numpy.set_printoptions(sign=' ')
+arr = numpy.array(input().split(), float)
+print(numpy.floor(arr))
+print(numpy.ceil(arr))
+print(numpy.rint(arr))
 
 # Task
 # You are given a 1-D array, A. Your task is to print the floor, ceil and rint of all the elements of A.
