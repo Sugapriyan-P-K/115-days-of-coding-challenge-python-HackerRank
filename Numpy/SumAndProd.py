99e36d89880c0e8b107039584df1921188f06a4a
SUGAPRIYAN P K <sugapriyan2003@gmail.com>
2023-08-17 19:20:25 +0530

    Create SumAndProd.py

---
+++ Numpy/SumAndProd.py
@@ -0,0 +1,14 @@
+
+
+# Task
+# You are given a 2-D array with dimensions N X M.
+# Your task is to perform the sum tool over axis 0 and then find the product of that result.
+# Sample Input
+# 2 2
+# 1 2
+# 3 4
+# Sample Output
+# 24
+# Explanation
+# The sum along axis 0 = [4 6]
+# The product of this sum = 24
