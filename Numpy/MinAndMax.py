2eb3da00d226450cba7cfcfb6153cb1458173986
SUGAPRIYAN P K <sugapriyan2003@gmail.com>
2023-08-18 20:09:46 +0530

    Create MinAndMax.py

---
+++ Numpy/MinAndMax.py
@@ -0,0 +1,16 @@
+
+
+# Task
+# You are given a 2-D array with dimensions N X M.
+# Your task is to perform the min function over axis 1 and then find the max of that.
+# Sample Input
+# 4 2
+# 2 5
+# 3 7
+# 1 3
+# 4 0
+# Sample Output
+# 3
+# Explanation
+# The min along axis 1 = [2,3,1,0]
+# The max of [2,3,1,0] = 3
