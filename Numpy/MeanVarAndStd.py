5514e537c0f703a388687c310f6ae557e3994132
SUGAPRIYAN P K <sugapriyan2003@gmail.com>
2023-08-19 10:23:14 +0530

    Update MeanVarAndStd.py

--- Numpy/MeanVarAndStd.py
+++ Numpy/MeanVarAndStd.py
@@ -1,4 +1,13 @@
-
+import numpy
+n,m = map(int, input().split())
+arr = numpy.array([input().strip().split() for _ in range(n)], int)
+print(numpy.mean(arr, axis=1))
+print(numpy.var(arr, axis=0))
+out = numpy.std(arr, axis=None)
+if  out==0:
+    print("{0:.1f}".format(out))
+else:
+    print("{0:.11f}".format(out))
 
 # Task
 # You are given a 2-D array of size N X M.
