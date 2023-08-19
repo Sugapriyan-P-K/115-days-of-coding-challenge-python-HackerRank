eced9d1487fafbf861ee0eda5a5274d0c81c446a
SUGAPRIYAN P K <sugapriyan2003@gmail.com>
2023-08-19 10:22:18 +0530

    Create MeanVarAndStd.py

---
+++ Numpy/MeanVarAndStd.py
@@ -0,0 +1,16 @@
+
+
+# Task
+# You are given a 2-D array of size N X M.
+# Your task is to find:
+# The mean along axis 1
+# The var along axis 0
+# The std along axis None
+# Sample Input
+# 2 2
+# 1 2
+# 3 4
+# Sample Output
+# [ 1.5  3.5]
+# [ 1.  1.]
+# 1.11803398875
