7487153966cda92c7a19ae4ae5124e2a9ae0f713
SUGAPRIYAN P K <sugapriyan2003@gmail.com>
2023-08-20 19:10:43 +0530

    Create DotAndCross.py

---
+++ Numpy/DotAndCross.py
@@ -0,0 +1,14 @@
+
+
+# Task
+# You are given two arrays A and B. Both have dimensions of N X N.
+# Your task is to compute their matrix product.
+# Sample Input
+# 2
+# 1 2
+# 3 4
+# 1 2
+# 3 4
+# Sample Output
+# [[ 7 10]
+#  [15 22]]
