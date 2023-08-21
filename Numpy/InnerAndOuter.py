092a3353ce3680615a1f92bd4295254d37bd6864
SUGAPRIYAN P K <sugapriyan2003@gmail.com>
2023-08-21 14:19:06 +0530

    Update InnerAndOuter.py

--- Numpy/InnerAndOuter.py
+++ Numpy/InnerAndOuter.py
@@ -1,4 +1,7 @@
-
+import numpy
+a1 = numpy.array(input().strip().split(), int)
+a2 = numpy.array(input().strip().split(), int)
+print(numpy.inner(a1,a2), numpy.outer(a1,a2), sep='\n')
 
 # Task
 # You are given two arrays: A and B.
