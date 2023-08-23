353a42e1778832fd16a88b16937469ef535b2016
SUGAPRIYAN P K <sugapriyan2003@gmail.com>
2023-08-23 12:09:39 +0530

    Create LinearAlgebra.py

---
+++ Numpy/LinearAlgebra.py
@@ -0,0 +1,10 @@
+
+
+# Task
+# You are given a square matrix A with dimensions N X N. Your task is to find the determinant. Note: Round the answer to 2 places after the decimal.
+# Sample Input
+# 2
+# 1.1 1.1
+# 1.1 1.1
+# Sample Output
+# 0.0
